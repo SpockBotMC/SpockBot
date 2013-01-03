8955a3874d8ba26218459ebf982afee89cb41602
Nick Gamberini <nickelpro@gmail.com>
2013-01-03 04:55:39 -0500

    .

--- mcp/bound_buffer.py
+++ mcp/bound_buffer.py
@@ -1,30 +1,33 @@
 class BufferUnderflowException(Exception):
-    pass
+	pass
 
 class BoundBuffer:
-    backup = ''
-    def __init__(self, *args):
-        self.buff = (args[0] if args else '')
-    
-    def read(self, bytes):
-        if len(self.buff) < bytes:
-            raise BufferUnderflowException()
-        o, self.buff = self.buff[:bytes], self.buff[bytes:]
-        return o
+	backup = ''
+	def __init__(self, *args):
+		self.buff = (args[0] if args else '')
+	
+	def read(self, bytes):
+		if len(self.buff) < bytes:
+			raise BufferUnderflowException()
+		o, self.buff = self.buff[:bytes], self.buff[bytes:]
+		return o
 
-    def append(self, bytes):
-        self.buff += bytes
-    
-    def flush(self):
-        out = self.buff
-        self.buff = ''
-        self.save()
-        return out
-    
-    def save(self):
-        self.backup = str(self.buff)
-    
-    def revert(self):
-        self.buff = str(self.backup)
-    
-    recv = read
+	def append(self, bytes):
+		self.buff += bytes
+	
+	def flush(self):
+		out = self.buff
+		self.buff = ''
+		self.save()
+		return out
+	
+	def save(self):
+		self.backup = str(self.buff)
+	
+	def revert(self):
+		self.buff = str(self.backup)
+
+	def close(self):
+		pass
+	
+	recv = read
