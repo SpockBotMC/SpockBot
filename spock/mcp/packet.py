0c9d6b4d9fd8d53395d09cbbc590753e1399aa1f
Nick Gamberini <nickelpro@gmail.com>
2013-01-28 00:01:55 -0500

    FUCK

--- spock/mcp/packet.py
+++ spock/mcp/packet.py
@@ -27,8 +27,6 @@ class Packet:
 		#Extension
 		if self.ident in packet_extensions.extensions:
 			packet_extensions.extensions[self.ident].decode_extra(self, bbuff)
-		print self.data
-
 	
 	def encode(self):
 		#Ident
