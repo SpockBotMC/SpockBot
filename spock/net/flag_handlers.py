da3d330120697fbecea99c3f40142892ef14a092
Nick Gamberini <nickg@nickg.org>
2013-02-03 16:35:19 -0500

    Event loop files

---
+++ spock/net/flag_handlers.py
@@ -0,0 +1,32 @@
+
+from spock.mcp.mcpacket import read_packet
+from spock.bound_buffer import BufferUnderflowException
+
+fhandles = {}
+def fhandle(ident):
+	def inner(cl):
+		fhandles[ident] = cl
+		return cl
+	return inner
+
+#SOCKET_RECV - Socket is ready to recieve data
+@fhandle(0x01)
+def handle01(client):
+	data = client.sock.recv(client.bufsize)
+	client.rbuff.append(client.decipher.decrypt(data) if client.encrypted else data)
+	client.rbuff.save()
+
+#SOCKET_SEND - Socket is ready to send data and Send buffer contains data to send
+@fhandle(0x02)
+def handle02(client):
+	sent = client.sock.send(client.sbuff)
+	client.sbuff = client.sbuff[sent:]
+
+#RBUFF_RECV - Read buffer has data ready to be unpacked
+@fhandle(0x04)
+def handle04(client):
+	try:
+		packet = read_packet(client.rbuff)
+		client.dispatch_packet(packet)
+	except BufferUnderflowException:
+		client.rbuff.revert()
