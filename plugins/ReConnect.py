bfaeaca03f5c96f10f97ba1ba7ca769ed54a2520
Nick Gamberini <nickg@nickg.org>
2013-02-05 16:35:32 -0500

    Cleanup

--- plugins/ReConnect.py
+++ plugins/ReConnect.py
@@ -9,7 +9,7 @@ class ReConnectPlugin:
 		client.register_dispatch(self.reconnect, 0xFF)
 		client.register_dispatch(self.grab_host, 0x02)
 	def reconnect(self, packet):
-		print "Attempting reconnect"
+		#print "Attempting reconnect"
 		self.client.login(self.host, self.port)
 	#Grabs host and port on handshake
 	def grab_host(self, packet):
