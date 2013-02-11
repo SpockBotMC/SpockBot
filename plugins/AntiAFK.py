cd2a77a1bfee3776576ca723a9ce7302ae37e982
Nick Gamberini <nickelpro@gmail.com>
2013-02-11 03:22:38 -0500

    Added Anti-AFK plugin

--- plugins/AntiAFK.py
+++ plugins/AntiAFK.py
@@ -1,16 +1,21 @@
 from spock.mcp.mcpacket import Packet
+from copy import copy
 
 #Very bad and naive Anti-AFK plugin
 class AntiAFKPlugin:
 	def __init__(self, client):
 		self.client = client
 		client.register_dispatch(self.avoid_afk, 0x03)
+		self.sentinal = True
 
 	def avoid_afk(self, packet):
 		msg = packet.data['text'].lower()
 		if ('afk' in msg) or ('inactivity' in msg):
-			oldposition = self.client.position
 			newposition = self.client.position
-			newposition['x']+=1
+			if self.sentinal:
+				newposition['x']+=1
+				self.sentinal = False
+			else:
+				newposition['x']-=1
+				self.sentinal = True		
 			self.client.push(Packet(ident=0x0D, data=newposition))
-			self.client.push(Packet(ident=0x0D, data=oldposition))
