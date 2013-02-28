2640470a3740106ba59ae4c2f87ac0663df457d2
Nick Gamberini <nickelpro@gmail.com>
2013-02-28 18:11:46 -0500

    Debug

--- plugins/NoLaggMonitor.py
+++ plugins/NoLaggMonitor.py
@@ -11,13 +11,14 @@ class NoLaggPlugin:
 			"text": "/nolagg stats"
 			})
 		self.stop_event = threading.Event()
+		self.timer = ThreadedTimer(self.stop_event, 40, self.check_nolagg, -1)
 		client.register_dispatch(self.start_timer, 0x02)
 		client.register_handler(self.stop_timer, cflags['SOCKET_ERR'], cflags['SOCKET_HUP'], cflags['KILL_EVENT'])
 		client.register_dispatch(self.stop_timer, 0xFF)
 
 	def start_timer(self, *args):
 		self.stop_event.clear()
-		ThreadedTimer(self.stop_event, 40, self.check_nolagg, -1).start()
+		self.timer.start()
 
 	def stop_timer(self, *args):
 		self.stop_event.set()
