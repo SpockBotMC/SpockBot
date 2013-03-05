6f0a7453616ec0777be1c6a051689f3f8e06f810
Nick Gamberini <nickg@nickg.org>
2013-03-05 02:41:28 -0500

    Remove daemon mode from demo

--- demo.py
+++ demo.py
@@ -3,5 +3,5 @@ from plugins import DebugPlugin, ReConnect, EchoPacket, Gravity, AntiAFK
 from login import username, password
 
 plugins = [ReConnect.ReConnectPlugin, DebugPlugin.DebugPlugin, AntiAFK.AntiAFKPlugin]
-client = Client(plugins = plugins, daemon = True)
+client = Client(plugins = plugins)
 client.start(username, password)
