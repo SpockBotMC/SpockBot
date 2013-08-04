57ba15de7f9f60362847fc444bddfcf10e86ac26
Nick Gamberini <nickelpro@gmail.com>
2013-08-04 15:30:28 -0400

    Removed unused side projects

--- tests/NBTtest.py
+++
@@ -1,9 +0,0 @@
-from spock.mcp import nbt
-from spock.bound_buffer import BoundBuffer
-
-magic = open('bigtest.nbt').read()
-
-tags = nbt.decode_nbt(magic)
-foo = nbt.encode_nbt(tags)
-tags = nbt.decode_nbt(foo)
-print tags.pretty_tree()
