7f5fea641849c239340e4286005c6d436334b9cf
Nick Gamberini <nickelpro@gmail.com>
2013-11-14 00:57:18 -0500

    Version Bump

--- setup.py
+++ setup.py
@@ -2,12 +2,14 @@ from distutils.core import setup
 
 setup (
     name='spock',
-    version='1.0',
+    version='1.1',
     packages=[
     	'spock', 
     	'spock.mcp', 
     	'spock.net', 
     	'spock.net.clients',
     	'spock.net.plugins',
+    	'spock.net.plugins.core',
+    	'spock.net.plugins.helpers',
     ],
 )
