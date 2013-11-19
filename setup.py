02e6467684d6460134235e04b997eb4fdd861596
Nick Gamberini <nickg@nickg.org>
2013-11-19 15:17:10 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from distutils.core import setup
 
 setup (
     name='spock',
-    version='1.1',
+    version='1.2,
     packages=[
     	'spock', 
     	'spock.mcp', 
-    	'spock.net', 
-    	'spock.net.clients',
+    	'spock.net',
     	'spock.net.plugins',
     	'spock.net.plugins.core',
     	'spock.net.plugins.helpers',
