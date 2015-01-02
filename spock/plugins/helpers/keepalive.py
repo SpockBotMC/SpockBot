7bcc856e50da2821c3bebf260ee1575c6c965a1c
Morgan Creekmore <gamingrobot@gmail.com>
2015-01-02 09:35:29 -0700

    Changed handle00 to handle_keep_alive

--- spock/plugins/helpers/keepalive.py
+++ spock/plugins/helpers/keepalive.py
@@ -5,10 +5,10 @@ class KeepalivePlugin:
 		self.net = ploader.requires('Net')
 		ploader.reg_event_handler(
 			(mcdata.PLAY_STATE, mcdata.SERVER_TO_CLIENT, 0x00),
-			self.handle00
+			self.handle_keep_alive
 		)
 
 	#Keep Alive - Reflects data back to server
-	def handle00(self, name, packet):
+	def handle_keep_alive(self, name, packet):
 		packet.new_ident((mcdata.PLAY_STATE, mcdata.CLIENT_TO_SERVER, 0x00))
 		self.net.push(packet)
