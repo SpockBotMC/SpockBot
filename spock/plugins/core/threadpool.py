1211df21c6c16df7ca2764ca9edf7769a3bd1c77
Nick Gamberini <nickelpro@gmail.com>
2015-01-02 10:06:44 -0500

    Taking out the trash
    These plugins were long due to be removed. They were bad ideas to begin
    with. Here's hoping I haven't forgotten anything important that uses
    them.

--- spock/plugins/core/threadpool.py
+++
@@ -1,8 +0,0 @@
-from concurrent.futures import ThreadPoolExecutor
-from spock.utils import pl_announce
-
-@pl_announce('ThreadPool')
-class ThreadPoolPlugin:
-	def __init__(self, ploader, settings):
-		w = ploader.requires('Settings')['thread_workers']
-		ploader.provides('ThreadPool', ThreadPoolExecutor(max_workers = w))
