1ee6e533652bccd8400dfafc8bc200719d610bba
Nick Gamberini <nickelpro@gmail.com>
2015-01-04 19:46:41 -0500

    Fallbacks are for cowards

--- spock/plugins/helpers/respawn.py
+++ spock/plugins/helpers/respawn.py
@@ -8,22 +8,12 @@ class RespawnPlugin:
 	def __init__(self, ploader, settings):
 		self.net = ploader.requires('Net')
 		if not ploader.requires('ClientInfo'):
-			ploader.reg_event_handler(
-				'PLAY<Update Health', self.handle_update_health_packet
-			)
-		else:
-			ploader.reg_event_handler(
-				'death', self.handle_death_event
-			)
+			print("RespawnPlugin requires ClientInfo, bailing out")
+	ploader.reg_event_handler(
+		'death', self.handle_death
+	)
 
-	#Update Health
-	def handle_update_health_packet(self, name, packet):
-		#You be dead
-		if packet.data['health'] <= 0.0:
-			self.net.push_packet(
-				'PLAY>Client Status', {'action': mcdata.CL_STATUS_RESPAWN}
-			)
-
+	#You be dead
 	def handle_death_event(self, name, data):
 		self.net.push_packet(
 			'PLAY>Client Status', {'action': mcdata.CL_STATUS_RESPAWN}
