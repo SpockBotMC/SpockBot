7ef6ed63f0ecc0437ed85eca6a0e43b1c7bf8f96
Morgan Creekmore <gamingrobot@gmail.com>
2015-01-08 09:12:09 -0700

    Added docs generator

---
+++ generatedocs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+pdoc --overwrite --all-submodules --html --html-dir /tmp/spockdocs ./spock
+git checkout gh-pages
+mv /tmp/spockdocs ./docs
+git add -A
+git commit -m "Updated docs"
+git push
+git checkout master
