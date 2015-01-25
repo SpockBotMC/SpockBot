49af1c8ef23002565bb7d466197dc6ea2e00d2df
Morgan Creekmore <gamingrobot@gmail.com>
2015-01-24 23:06:23 -0700

    Added click_window function

--- spock/plugins/helpers/inventory.py
+++ spock/plugins/helpers/inventory.py
@@ -2,6 +2,7 @@
 Inventory plugin is a helper plugin for dealing with interaction with inventories
 """
 
+from collections import deque
 from spock.utils import pl_announce
 from spock.mcp import mcdata, mcpacket
 
@@ -10,6 +11,16 @@ class InventoryCore:
 		self.clinfo = clinfo
 		self.net = net
 		self.action = 0
+		self.clickque = deque()
+	
+	def click_window(slot, button, mode):
+		self.clickque.append({
+			'window_id': 0,
+			'slot': slot,
+			'button': button,
+			'mode': mode,
+			'clicked_item': self.clinfo.inventory.slots[slot],
+		})
 	
 	def test_inventory(self):
 		for i in range(9,43):
@@ -45,5 +56,9 @@ class InventoryPlugin:
 		self.net = ploader.requires('Net')
 		self.inventory = InventoryCore(self.clinfo, self.net)
 		ploader.provides('Inventory', self.inventory)
-	
+		ploader.reg_event_handler('event_tick', self.tick)
+
+	def tick(self, event, data):
+		pass
+
 
