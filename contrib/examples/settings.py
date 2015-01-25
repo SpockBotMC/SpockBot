d36aa0d054d466b60e4bbd8ef22d6f25b5dc7157
Nick Gamberini <nickelpro@gmail.com>
2015-01-25 03:42:40 -0500

    Forgot to move files

---
+++ contrib/examples/settings.py
@@ -0,0 +1,12 @@
+from spock.plugins.defaults import DefaultPlugins
+
+default_settings = {
+	'username': 'Bot',     #minecraft.net username or name for unauthenticated servers
+	'password': '',	       #Password for account, ignored if not authenticated
+	'authenticated': True, #Authenticate with authserver.mojang.com
+	'bufsize': 4096,       #Size of socket buffer
+	'sock_quit': True,     #Stop bot on socket error or hangup
+	'sess_quit': True,     #Stop bot on failed session login
+	'plugins': DefaultPlugins,         #Plugins
+	'plugin_settings': {}, #Extra settings for plugins
+}
---
+++ contrib/plugins/ChatCommand.py
@@ -0,0 +1,62 @@
+from spock.mcp import mcdata
+
+class ChatCommandPlugin:
+	def __init__(self, ploader, settings):
+		self.physics = ploader.requires('Physics')
+		self.net = ploader.requires('Net')
+		self.inventory = ploader.requires('Inventory')
+		ploader.reg_event_handler(
+			'PLAY<Chat Message', self.handle_chat_message
+		)
+
+	def handle_chat_message(self, name, packet):
+		chat_data = packet.data['json_data']
+		message = self.parse_chat(chat_data)
+		print('Chat:', message)
+		try:
+			command = message[message.index('!'):]
+			args = []
+			spacepos = command.find(' ')
+			if spacepos == -1: #no arguments
+				command = command[1:]
+			else: #have arguments
+				args = command[spacepos+1:].split(' ')
+				command = command[1:spacepos]
+			self.command_handle(command.strip(), args)
+		except ValueError: #not a command so just move along
+			pass
+
+	def command_handle(self, command, args):
+		if command == '':
+			return
+		print("Command:", command)
+		if command == 'jump' or command == 'j':
+			self.physics.jump()
+		elif command == 'say':
+			#self.net.push_packet('PLAY>Chat Message', {'message': ' '.join(args)})
+			pass
+		elif command == 'cmd':
+			self.net.push_packet('PLAY>Chat Message', {'message': '/' + ' '.join(args)})
+		elif command == 'slot':
+			if len(args) == 1 and (int(args[0]) >= 0 and int(args[0]) <= 8):
+				self.net.push_packet('PLAY>Held Item Change', {'slot': int(args[0])})
+		elif command == 'inv':
+			self.inventory.test_inventory()
+
+	def parse_chat(self, chat_data):
+		message = ''
+		if type(chat_data) is dict:
+			if 'text' in chat_data:
+				message += chat_data['text']
+				if 'extra' in chat_data:
+					message += self.parse_chat(chat_data['extra'])
+			elif 'translate' in chat_data:
+				if 'with' in chat_data:
+					message += self.parse_chat(chat_data['with'])
+		elif type(chat_data) is list:
+			for text in chat_data:
+				if type(text) is str:
+					message += text
+				elif type(text) is dict:
+					message += self.parse_chat(text)
+		return message
