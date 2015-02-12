13e55847c34709910d2b25c4ef5e6f6cb9a9b9f9
Gjum <gjum42@gmail.com>
2015-02-12 15:15:58 +0100

    Add inventory classes

--- spock/plugins/helpers/inventory.py
+++ spock/plugins/helpers/inventory.py
@@ -16,46 +16,7 @@ from collections import deque
 from spock.utils import pl_announce
 
 import logging
-
 logger = logging.getLogger('spock')
-
-# TODO move all this minecraft data stuff elsewhere?
-
-# -1 is the internal entry for the player's inventory.
-# It has no official ID, because it is never opened by the server.
-# TODO add more slot indices (crafting/trading/enchanting/...)
-window_dict = {
-	-1: {"id": "minecraft:main", "name": "Inventory",
-	     "out_index": 0, "crafting_index": 1, "armor_index": 5, "inventory_index": 9, "hotbar_index": 36},
-	 0: {"id": "minecraft:chest", "name": "Chest/Large chest"},
-	 1: {"id": "minecraft:crafting_table", "name": "Workbench",
-	     "out_index": 0, "crafting_index": 1, "inventory_index": 10, "hotbar_index": 37},
-	 2: {"id": "minecraft:furnace", "name": "Furnace",
-	     "smelted_index": 0, "fuel_index": 1, "out_index": 2, "inventory_index": 3, "hotbar_index": 30},
-	 3: {"id": "minecraft:dispenser", "name": "Dispenser"},
-	 4: {"id": "minecraft:enchanting_table", "name": "Enchantment table"},
-	 5: {"id": "minecraft:brewing_stand", "name": "Brewing Stand"},
-	 6: {"id": "minecraft:villager", "name": "Npc trade"},
-	 7: {"id": "minecraft:beacon", "name": "Beacon"},
-	 8: {"id": "minecraft:anvil", "name": "Anvil"},
-	 9: {"id": "minecraft:hopper", "name": "Hopper"},
-	10: {"id": "minecraft:dropper", "name": "Dropper"},
-	11: {"id": "EntityHorse", "name": "Horse"},
-}
-
-INV_MAIN = -1
-INV_CHEST = 0
-INV_WORKBENCH = 1
-INV_FURNACE = 2
-INV_DISPENSER = 3
-INV_ECHANTMENT = 4
-INV_BREWING = 5
-INV_NPC = 6
-INV_BEACON = 7
-INV_ANVIL = 8
-INV_HOPPER = 9
-INV_DROPPER = 10
-INV_HORSE = 11
 
 INV_BUTTON_LEFT = 0
 INV_BUTTON_RIGHT = 1
@@ -69,7 +30,7 @@ INV_MODE_DROP = 4
 INV_MODE_PAINT = 5
 INV_MODE_DOUBLECLICK = 6
 
-INV_WINID_MAIN = 0  # main inventory window ID, always open
+INV_WINID_PLAYER = 0  # main inventory window ID, always open
 
 class Slot:
 	def __init__(self, id=-1, amount=0, damage=0, enchants=None):
@@ -89,102 +50,247 @@ class Slot:
 			data['enchants'] = self.enchants
 		return data
 
-class Window:
-	def __init__(self, window_id, inv_type, title, slot_count, eid=None):
+inv_types = {}
+def map_window_type(inv_type_id):
+	def inner(cl):
+		inv_types[inv_type_id] = cl
+		cl.inv_type = inv_type_id
+		return cl
+	return inner
+
+class InventoryBase:
+	""" Base class for all inventory types. """
+
+	def __init__(self, inv_type, window_id, title, slot_count):
+		self.inv_type = inv_type  # unused
 		self.window_id = window_id
-		self.inv_type = inv_type
 		self.title = title
 		self.slots = [Slot()] * slot_count
+		self.properties = {}
+
+	# TODO REMOVE
+	# def __getattr__(self, attr_name):
+	# 	""" Override default for get_slots_hotbar() etc. """
+	# 	prefix = 'get_slots_'
+	# 	if attr_name.startswith(prefix): # get slot range
+	# 		slot_startlen_name = 'slot_startlen_%s' % attr_name[len(prefix):] # without prefix
+	# 		if slot_startlen_name in dir(self):
+	# 			slot_start, slot_len = getattr(self, slot_startlen_name)
+	# 			return self.slots[slot_start:slot_start+slot_len]
+	# 		else: # failed, try regular property
+	# 			return getattr(self, attr_name)
+	# TODO REMOVE
+
+# no @map_window_type(), because not opened/closed/updated by the server
+class InventoryPlayer(InventoryBase):
+	name = 'Inventory'
+
+	def __init__(self):
+		super().__init__(-1, INV_WINID_PLAYER, 'Inventory', 45)
+
+	def craft_result_slot(self):
+		return self.slots[0]
+
+	def craft_grid_slots(self):
+		return self.slots[1:5]
+
+	def armor_slots(self):
+		return self.slots[5:9]
+
+	def inventory_slots(self):
+		return self.slots[9:36]
+
+	def hotbar_slots(self):
+		return self.slots[36:45]
+
+@map_window_type(0)
+class InventoryChest(InventoryBase):
+	""" Small, large, and glitched-out superlarge chests. """
+
+	name = 'Chest'
+
+@map_window_type(1)
+class InventoryWorkbench(InventoryBase):
+	name = 'Workbench'
+
+	def craft_result_slot(self):
+		return self.slots[0]
+
+	def craft_grid_slots(self):
+		return self.slots[1:10]
+
+@map_window_type(2)
+class InventoryFurnace(InventoryBase):
+	name = 'Furnace'
+
+	def smelted_slot(self):
+		return self.slots[0]
+
+	def fuel_slot(self):
+		return self.slots[1]
+
+	def result_slot(self):
+		return self.slots[2]
+
+	def progress_prop(self):
+		return self.properties[0]
+
+	def fuel_time_prop(self):
+		return self.properties[1]
+
+@map_window_type(3)
+class InventoryDispenser(InventoryBase):
+	name = 'Dispenser'
+
+@map_window_type(4)
+class InventoryEnchant(InventoryBase):
+	name = 'Encantment Table'
+
+	def enchanted_slot(self):
+		return self.slots[0]
+
+	def lapis_slot(self):
+		return self.slots[1]
+
+	# TODO how does selecting the enchantment work?
+
+@map_window_type(5)
+class InventoryBrewing(InventoryBase):
+	name = 'Brewing Stand'
+
+	def ingredient_slot(self):
+		return self.slots[0]
+
+	def result_slots(self):
+		return self.slots[1:4]
+
+	def brew_time_prop(self):
+		return self.properties[0]
+
+@map_window_type(6)
+class InventoryNpc(InventoryBase):
+	name = 'NPC Trade'
+
+# TODO NPC slot getters
+
+@map_window_type(7)
+class InventoryBeacon(InventoryBase):
+	name = 'Beacon'
+
+	def input_slot(self):
+		return self.slots[0]
+
+	def level_prop(self):
+		return self.properties[0]
+
+	def effect_one_prop(self):
+		return self.properties[1]
+
+	def effect_two_prop(self):
+		return self.properties[2]
+
+@map_window_type(8)
+class InventoryAnvil(InventoryBase):
+	name = 'Anvil'
+
+# TODO anvil slot getters
+
+	def max_cost_prop(self):
+		return self.properties[0]
+
+@map_window_type(9)
+class InventoryHopper(InventoryBase):
+	name = 'Hopper'
+
+@map_window_type(10)
+class InventoryDropper(InventoryBase):
+	name = 'Dropper'
+
+@map_window_type(11)
+class InventoryHorse(InventoryBase):
+	name = 'Horse'
+
+	def __init__(self, eid=0, **args):
+		super().__init__(**args)
 		self.horse_entity_id = eid
 
-	def set_slot(self, slot_nr, **slot_data):
-		self.slots[slot_nr] = Slot(**slot_data)
+	# TODO horse slot getters
 
-	def get_info(self):
-		return window_dict[self.inv_type]
-
-class InventoryCore(dict):
-	""" Handles all open windows and their slots.
-	Instance is a dict of `Window ID -> Window instance`. """
+class InventoryCore:
+	""" Handles the player inventory and the last opened window. """
 
 	def __init__(self, net, send_click):
-		super().__init__()  # initialize window dict
 		self.net = net
 		self._send_click = send_click  # method to queue a click action
 		self.selected_slot = 0
 		# initialize main inventory, always open, always same ID
-		self[INV_WINID_MAIN] = Window(INV_WINID_MAIN, -1, 'Inventory', 45)
+		self.player = InventoryPlayer()
+		self.window = None
 
-	def hold_item(self, wanted_item_id, wanted_item_meta=-1):
-		""" Reurns True if a stack of the specified item ID
+	def hold_item(self, wanted_id, wanted_meta=-1):
+		""" Returns True if a stack of the specified item ID
 		could be placed in the hotbar and selected,
 		False otherwise. """
 
 		def wanted(slot):  # check if the slot contains the wanted stack
 			slot_id, slot_meta = slot.item_id, slot.damage
-			return wanted_item_id == slot_id and wanted_item_meta in (-1, slot_meta)
+			return wanted_id == slot_id and wanted_meta in (-1, slot_meta)
 
-		slot = self.get_hotbar()[self.selected_slot]
+		slot = self.player.hotbar_slots()[self.selected_slot]
 		if wanted(slot): return True # already selected
 		# not selected, search for it
 		# hotbar is at the end of the inventory, search there first
-		for slot_nr, slot in enumerate(self.get_hotbar()):
+		for slot_nr, slot in enumerate(self.player.hotbar_slots()):
 			if wanted(slot):
 				self.select_slot(slot_nr)
 				return True
 		# not in hotbar, search inventory
-		# TODO search all slots in all open windows
-		for slot_nr, slot in enumerate(self.get_inventory()):
+		for slot_nr, slot in enumerate(self.player.slots):
 			if wanted(slot):
 				self.swap_into_hotbar(slot_nr)
 				return True
+		# not in inventory, search open window
+		for slot_nr, slot in enumerate(self.window.slots):
+			if wanted(slot):
+				self.swap_into_hotbar(slot_nr, player_inv=False)
+				return True
 		return False
-
-	def get_hotbar(self):
-		return self[INV_WINID_MAIN].slots[-9:]  # hotbar is always at the end of the window
-
-	def get_inventory(self):
-		return self[INV_WINID_MAIN].slots[:-9]  # exclude hotbar
 
 	def select_slot(self, slot_nr):
 		if 0 <= slot_nr < 9:
 			self.selected_slot = slot_nr
 			self.net.push_packet('PLAY>Held Item Change', {'slot': slot_nr})
 
-	def shift_click(self, slot, window_id=None):
-		self.click_window(slot, INV_BUTTON_LEFT, INV_MODE_SHIFT, window_id)
+	def shift_click(self, slot, player_inv=True):
+		self.click_window(slot, INV_BUTTON_LEFT, INV_MODE_SHIFT, player_inv)
 
-	def swap_into_hotbar(self, slot, hotbar_slot=None, window_id=None):
+	def swap_into_hotbar(self, slot, hotbar_slot=None, player_inv=True):
 		if hotbar_slot is None: hotbar_slot = self.selected_slot
-		self.click_window(slot, hotbar_slot, INV_MODE_SWAP_HOTBAR, window_id)
+		self.click_window(slot, hotbar_slot, INV_MODE_SWAP_HOTBAR, player_inv)
 
-	def swap_slots(self, slot_a, slot_b, window_id=None):
-		""" Swaps the two slots in the specified window. """
+	def swap_slots(self, slot_a, slot_b, player_inv=True):
 		# swap A into hotbar
-		self.swap_into_hotbar(slot_a, 0, window_id)
+		self.swap_into_hotbar(slot_a, 0, player_inv)
 		# swap A with B, B then is in hotbar
-		self.swap_into_hotbar(slot_b, 0, window_id)
+		self.swap_into_hotbar(slot_b, 0, player_inv)
 		# swap B to A's original position, restore hotbar
-		self.swap_into_hotbar(slot_a, 0, window_id)
+		self.swap_into_hotbar(slot_a, 0, player_inv)
 
-	def click_window(self, slot, button, mode, window_id=None):
-		if window_id is None: window_id = self.get_newest_open_window_id()
+	def click_window(self, slot, button, mode, player_inv=True):
+		window = self.player if player_inv else self.window
 		# make sure slot is in inventory,
 		# allows for slot = -9 as first slot of hotbar etc.
-		slot_count = len(self[window_id].slots)
+		slot_count = len(window.slots)
 		slot += slot_count
 		slot %= slot_count
 		# action ID gets added in _send_click
 		self._send_click({
-			'window_id': window_id,
+			'window_id': window.window_id,
 			'slot': slot,
 			'button': button,
 			'mode': mode,
 			'clicked_item': {'id': -1},
 		})
-
-	def get_newest_open_window_id(self):
-		return max(self.keys())
 
 @pl_announce('Inventory')
 class InventoryPlugin:
@@ -192,8 +298,8 @@ class InventoryPlugin:
 		self.clinfo = ploader.requires('ClientInfo')
 		self.event = ploader.requires('Event')
 		self.net = ploader.requires('Net')
-		self.inventories = InventoryCore(self.net, self.send_click)
-		ploader.provides('Inventory', self.inventories)
+		self.inventory = InventoryCore(self.net, self.send_click)
+		ploader.provides('Inventory', self.inventory)
 
 		# Inventory events
 		ploader.reg_event_handler(
@@ -217,36 +323,32 @@ class InventoryPlugin:
 		self.last_click = None  # stores the last click action for confirmation
 
 	def handle_held_item_change(self, event, packet):
-		self.inventories.selected_slot = packet.data['slot']
+		self.inventory.selected_slot = packet.data['slot']
 		self.event.emit('inv_held_item_change', packet.data)
 
 	def handle_open_window(self, event, packet):
-		self.inventory[packet.data['window_id']] = Window(**packet.data['window_id'])
-		self.event.emit('inv_open_window', packet.data)
+		logger.debug('open window %s', packet.data)  # TODO remove
+		self.inventory.window = inv_types[packet.data['inv_type']](**packet.data)
+		self.event.emit('inv_open_window', {'window': self.inventory.window})
 
 	def handle_close_window(self, event, packet):
-		if packet.data['window_id'] not in self.inventory:
-			logger.error('[Inventory] Could not close %s, ', packet.data['window_id'])
-			return
-		del self.inventory[packet.data['window_id']]
-		self.event.emit('inv_close_window', packet.data)
+		self.event.emit('inv_close_window', {'window': self.inventory.window})
+		self.inventory.window = None
 
 	def handle_set_slot(self, event, packet):
 		window_id = packet.data['window_id']
 		# Notchian server sends window_id = slot_nr = -1 on /clear and login
-		if window_id != -1:
-			self.inventories[window_id].set_slot(packet.data['slot'], **packet.data['slot_data'])
-			self.event.emit('inv_set_slot', packet.data)
-		else:
+		if window_id == -1:
 			self.event.emit('inv_clear_window', packet.data)
+		else:
+			self.set_slot_in_window(window_id, packet.data['slot'], **packet.data['slot_data'])
 
 	def handle_window_items(self, event, packet):
 		for slot_nr, slot_data in enumerate(packet.data['slots']):
-			self.inventories[packet.data['window_id']].set_slot(slot_nr, **slot_data)
-			self.event.emit('inv_set_slot', slot_data)
+			self.set_slot_in_window(packet.data['window_id'], slot_nr, **slot_data)
 
 	def handle_window_prop(self, event, packet):
-		# TODO packet has 'window_id', 'property', 'value', all numbers, dependent on window type
+		self.get_window_from_id(packet.data['window_id']).properties[packet.data['property']] = packet.data['value']
 		self.event.emit('inv_win_prop', packet.data)
 
 	def handle_confirm_transact(self, event, packet):
@@ -278,6 +380,18 @@ class InventoryPlugin:
 	def get_next_action_id(self):
 		self.action_id += 1
 		return self.action_id
+
+	def get_window_from_id(self, window_id):
+		return self.inventory.player if window_id == INV_WINID_PLAYER else self.inventory.window
+
+	def set_slot_in_window(self, window_id, slot_nr, **slot_data):
+		window = self.get_window_from_id(window_id)
+		window.slots[slot_nr] = Slot(**slot_data)
+		data = {
+			'window': window,
+			'slot_nr': slot_nr,
+		}.update(slot_data)
+		self.event.emit('inv_set_slot', data)
 
 	def update_slots(self, click):
 		pass  # FIXME IMPORTANT change the slots in main inventory accordingly
