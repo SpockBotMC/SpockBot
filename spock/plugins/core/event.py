60746e50fce47bb7bd1333019323f28130233128
Gjum <gjum42@gmail.com>
2015-03-22 03:06:54 +0100

    Use defaultdict

--- spock/plugins/core/event.py
+++ spock/plugins/core/event.py
@@ -1,10 +1,10 @@
 """
 Provides the core event loop
 """
+from collections import defaultdict
 
 import signal
 import copy
-from spock.mcp import mcdata
 from spock.utils import pl_announce
 
 import logging
@@ -13,7 +13,7 @@ logger = logging.getLogger('spock')
 class EventCore:
 	def __init__(self):
 		self.kill_event = False
-		self.event_handlers = {}
+		self.event_handlers = defaultdict(list)
 		signal.signal(signal.SIGINT, self.kill)
 		signal.signal(signal.SIGTERM, self.kill)
 
@@ -24,13 +24,9 @@ class EventCore:
 		self.emit('kill')
 
 	def reg_event_handler(self, event, handler):
-		if event not in self.event_handlers:
-			self.event_handlers[event] = []
 		self.event_handlers[event].append(handler)
 
 	def emit(self, event, data = None):
-		if event not in self.event_handlers:
-			self.event_handlers[event] = []
 		to_remove = []
 		for handler in self.event_handlers[event]:
 			if handler(
