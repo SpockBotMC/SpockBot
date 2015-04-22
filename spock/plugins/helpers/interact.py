107baccb6eb6bcf5526db19d41e8ca5c93de2816
Gjum <gjum42@gmail.com>
2015-04-23 00:10:46 +0200

    Fix interact, improve usability

--- spock/plugins/helpers/interact.py
+++ spock/plugins/helpers/interact.py
@@ -1,5 +1,5 @@
 """
-Interact with the world.
+Interact with the world:
 - swing the arm, sneak, sprint, jump with a horse, leave the bed
 - chat, whisper (private message)
 - look around
@@ -7,9 +7,14 @@ Interact with the world.
 - use the held (active) item
 - use/attack entities
 - steer vehicles
+
+By default, the client sends swing and look packets like the vanilla client.
+This can be disabled by setting the auto_swing and auto_look flags.
 """
 import math
 from spock.utils import pl_announce, Vec3
+
+PLAYER_HEIGHT = 1.74
 
 INTERACT_ENTITY = 0
 ATTACK_ENTITY = 1
@@ -31,20 +36,32 @@ FACE_Z_POS = 1
 FACE_X_NEG = 0
 FACE_X_POS = 1
 
+DIG_START = 0
+DIG_CANCEL = 1
+DIG_FINISH = 2
+DIG_DROP_STACK = 3
+DIG_DROP_ITEM = 4
+DIG_DEACTIVATE_ITEM = 5
+
 @pl_announce('Interact')
 class InteractPlugin:
 	def __init__(self, ploader, settings):
-		self.clinfo = ploader.requires('Clientinfo')
+		self.clinfo = ploader.requires('ClientInfo')
+		self.entities = ploader.requires('Entities')
 		self.inventory = ploader.requires('Inventory')
 		self.net = ploader.requires('Net')
 		ploader.provides('Interact', self)
 
 		self.sneaking = False
-		self.noswing = False  # set to True to prevent arm movement
+		self.sprinting = False
+
+		self.auto_swing = True  # move arm when clicking
+		self.auto_look = True  # look at clicked things
+
+		self.dig_pos_dict = {'x': 0, 'y': 0, 'z': 0}
 
 	def swing_arm(self):
-		if not self.noswing:
-			self.net.push_packet('PLAY>Animation', {})
+		self.net.push_packet('PLAY>Animation', {})
 
 	def _entity_action(self, action, jump_boost=100):
 		entity_id = self.clinfo.eid
@@ -59,7 +76,7 @@ class InteractPlugin:
 
 	def sneak(self, sneak=True):
 		self._entity_action(ENTITY_ACTION_SNEAK
-						   if sneak else ENTITY_ACTION_UNSNEAK)
+		                    if sneak else ENTITY_ACTION_UNSNEAK)
 		self.sneaking = sneak
 
 	def unsneak(self):
@@ -67,7 +84,8 @@ class InteractPlugin:
 
 	def sprint(self, sprint=True):
 		self._entity_action(ENTITY_ACTION_START_SPRINT
-						   if sprint else ENTITY_ACTION_STOP_SPRINT)
+		                    if sprint else ENTITY_ACTION_STOP_SPRINT)
+		self.sprinting = sprint
 
 	def unsprint(self):
 		self.sprint(False)
@@ -81,121 +99,139 @@ class InteractPlugin:
 	def whisper(self, player, message):
 		self.chat('/tell %s %s' % (player, message))
 
-	def look(self, yaw=0, pitch=0):
+	def look(self, yaw=0.0, pitch=0.0):
+		"""
+		Turn the head. Both angles are in degrees.
+		"""
 		self.clinfo.position.pitch = pitch
 		self.clinfo.position.yaw = yaw
 		self.net.push_packet('PLAY>Player Look', {
-			'yaw': yaw,
-			'pitch': pitch,
+			'yaw': int(yaw),
+			'pitch': int(pitch),
 			'on_ground': self.clinfo.position.on_ground
 		})
 
 	def look_relative(self, yaw=0, pitch=0):
-		self.look_at(self.clinfo.position.yaw + yaw,
-					 self.clinfo.position.pitch + pitch)
+		self.look(self.clinfo.position.yaw + yaw,
+		          self.clinfo.position.pitch + pitch)
 
 	def look_at(self, pos):
 		delta_x = pos.x - self.clinfo.position.x
-		delta_y = pos.y - self.clinfo.position.y + 1.7  # player height
+		delta_y = pos.y - self.clinfo.position.y + PLAYER_HEIGHT
 		delta_z = pos.z - self.clinfo.position.z
-		groundDistance = math.sqrt(delta_x * delta_x + delta_z * delta_z)
-		pitch = math.atan2(delta_y, groundDistance) * 180 / math.pi
+		ground_distance = math.sqrt(delta_x * delta_x + delta_z * delta_z)
+		pitch = math.atan2(delta_y, ground_distance) * 180 / math.pi
 		yaw = math.atan2(-delta_x, -delta_z) * 180 / math.pi
 		self.look(yaw, pitch)
 
-	def dig_block(self, coords):
-		self.dig_coords_dict = coords.get_dict().copy()
-		packet = {
-			'status': 0,  # start
-			'location': self.dig_coords_dict,
-			'face': 1,
-		}
-		self.net.push_packet('PLAY>Player Digging', packet)
+	def _send_dig_block(self, status, pos=None, face=FACE_Y_POS):
+		if status == DIG_START:
+			self.dig_pos_dict = pos.get_dict().copy()
+		self.net.push_packet('PLAY>Player Digging', {
+			'status': status,
+			'location': self.dig_pos_dict,
+			'face': face,
+		})
 
-	def stop_digging(self):
-		if not self.dig_coords_dict:
-			# TODO raise or return?
-			raise AssertionError('dig_block() must be called before stop_digging()')
-		packet = {
-			'status': 2,  # finish
-			'location': self.dig_coords_dict,
-			'face': 1,
-		}
-		self.net.push_packet('PLAY>Player Digging', packet)
+	def start_digging(self, pos):
+		if self.auto_look: self.look_at(pos)  # TODO look at block center
+		self._send_dig_block(DIG_START, pos)
+		if self.auto_swing: self.swing_arm()  # TODO send swing animation until done or stopped
 
-	def place_block(self, coords, face=1, cursor_pos=Vec3(8,8,8)):
+	def cancel_digging(self):
+		self._send_dig_block(DIG_CANCEL)
+
+	def finish_digging(self):
+		self._send_dig_block(DIG_FINISH)
+
+	def dig_block(self, pos):
 		"""
-		Place a block next to coords, or
-		at coords if the block at coords is air.
+		Not cancelable.
 		"""
-		sneaking_before = self.sneaking
-		self.sneak()
-		self.use_block(coords, face, cursor_pos)
-		self.sneak(sneaking_before)
+		self.start_digging(pos)
+		self.finish_digging()
 
-	def click_block(self, coords, face=1, cursor_pos=Vec3(8,8,8)):
+	def click_block(self, pos, face=1, cursor_pos=Vec3(8,8,8), look_at_block=True):
 		"""
 		Click on a block, e.g. push button, open window, make redstone ore glow.
 		:param face: side of the reference block on which the block is placed on
-		:param cursor_pos: each dimension 0..15, where to click inside the block
+		:param cursor_pos: where to click inside the block, each dimension 0..15
 		"""
+		if look_at_block and self.auto_look: self.look_at(pos)  # TODO look at cursor_pos
 		self.net.push_packet('PLAY>Player Block Placement', {
-			'location': coords.get_dict(),
+			'location': pos.get_dict(),
 			'direction': face,
 			'held_item': self.inventory.active_slot.get_dict(),
-			'cur_pos_x': cursor_pos.x,
-			'cur_pos_y': cursor_pos.y,
-			'cur_pos_z': cursor_pos.z,
+			'cur_pos_x': int(cursor_pos.x),
+			'cur_pos_y': int(cursor_pos.y),
+			'cur_pos_z': int(cursor_pos.z),
 		})
-		self.swing_arm()
+		if self.auto_swing: self.swing_arm()
+
+	def place_block(self, pos, face=1, cursor_pos=Vec3(8,8,8), look_at_block=True, sneak=True):
+		"""
+		Place a block next to pos. If the block at pos is air, place at pos.
+		"""
+		sneaking_before = self.sneaking
+		if sneak: self.sneak()
+		self.click_block(pos, face, cursor_pos, look_at_block)
+		if sneak: self.sneak(sneaking_before)
+
+	def use_bucket(self, pos):  # TODO
+		"""
+		Using buckets is different from placing blocks.
+		See "Special note on using buckets" in http://wiki.vg/Protocol#Player_Block_Placement
+		"""
+		raise NotImplementedError
 
 	def activate_item(self):
 		"""
 		Use (hold right-click) the item in the active slot.
 		Examples: pull the bow, start eating once, throw an egg.
 		"""
-		self.use_block(Vec3(-1, 255, -1), 1, Vec3(-1, -1, -1))
+		self.click_block(Vec3(-1, 255, -1),
+						 face=1,
+						 cursor_pos=Vec3(-1, -1, -1),
+						 look_at_block=False)
 
 	def deactivate_item(self):
 		"""
 		Stop using (release right-click) the item in the active slot.
 		Examples: shoot the bow, stop eating.
 		"""
-		self.net.push_packet('PLAY>Player Digging', {
-			'status': 5,
-			'location': Vec3(0, 0, 0).get_dict(),
-			'face': 255,
-		})
+		self._send_dig_block(DIG_DEACTIVATE_ITEM)
 
-	def use_entity(self, entity_id, cursor_pos=Vec3(0.0, 0.0, 0.0), action=INTERACT_ENTITY):
+	def use_entity(self, entity, cursor_pos=None, action=INTERACT_ENTITY):
 		"""
 		Uses (right-click) an entity to open its window.
-		`action` is one of 0: interact, 1: attack, 2: interact at
+		Setting `cursor_pos` sets `action` to "interact at".
 		"""
-		packet = {'target': entity_id, 'action': action}
+		if self.auto_look: self.look_at(entity)  # TODO look at cursor_pos
+		if cursor_pos is not None:
+			action = INTERACT_ENTITY_AT
+		packet = {'target': entity.eid, 'action': action}
 		if action == INTERACT_ENTITY_AT:
 			packet['target_x'] = cursor_pos.x
 			packet['target_y'] = cursor_pos.y
 			packet['target_z'] = cursor_pos.z
 		self.net.push_packet('PLAY>Use Entity', packet)
-		self.swing_arm()
+		if self.auto_swing: self.swing_arm()
 
-	def attack_entity(self, entity_id):
-		self.use_entity(entity_id, action=ATTACK_ENTITY)
+	def attack_entity(self, entity):
+		self.use_entity(entity, action=ATTACK_ENTITY)
 
-	def mount_vehicle(self, entity_id):
-		self.use_entity(entity_id)
+	def mount_vehicle(self, entity):
+		self.use_entity(entity)
 
 	def steer_vehicle(self, sideways=0.0, forward=0.0, jump=False, unmount=False):
 		flags = 0
 		if jump: flags += 1
 		if unmount: flags += 2
-		packet = {
+		self.net.push_packet('PLAY>Steer Vehicle', {
 			'sideways': sideways,
 			'forward': forward,
 			'flags': flags,
-		}
-		self.net.push_packet('PLAY>Steer Vehicle', packet)
+		})
 
 	def unmount_vehicle(self):
 		self.steer_vehicle(unmount=True)
