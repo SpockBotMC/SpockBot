7966a9e3e12547cfa5334ca9208117f20b469c55
Jasper Bok <hello@jasperbok.nl>
2015-05-13 01:56:34 +0200

    Defined an initial version for the BasePlugin class
    BasePlugin allows for a more declarative syntax when creating plugins, removing the manual loading of requirements and settings, and automatically registering event handlers.

---
+++ spock/plugins/plugin_base.py
@@ -0,0 +1,21 @@
+from ..utils import get_settings
+
+class BasePlugin(object):
+	requires = ()
+	defaults = {}
+	events = {}
+
+	def __init__(self, ploader, settings):
+		# Load the plugin's settings.
+		self.settings = get_settings(self.defaults, settings)
+
+		# Load all the plugin's dependencies.
+		for requirement in self.requires:
+			setattr(self, requirement.lower(), ploader.requires(requirement))
+
+		# Setup the plugin's event handlers.
+		for event in self.events.keys():
+			if hasattr(self, self.events[event]):
+				ploader.reg_event_handler(event, getattr(self, self.events[event]))
+			else:
+				pass  # Do something useful here, like logging it.
