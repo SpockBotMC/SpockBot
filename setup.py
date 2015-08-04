a9ce72852deeb960ea6130c9bcad49e5dda82a54
Morgan Creekmore <gamingrobot@gmail.com>
2015-08-04 15:21:54 -0600

    Added pypi goodies

--- setup.py
+++ setup.py
@@ -3,14 +3,16 @@ from setuptools import find_packages
 
 setup (
     name='spock',
-    description='Minecraft library in python',
+    description='A pure python framework that implements the 1.8 Minecraft protocol for building Minecraft clients',
     license='MIT',
-    long_description=open('README.md').read(),
+    long_description=open('README.rst').read(),
     version='0.1.2',
+    url='https://github.com/SpockBotMC/SpockBot',
     packages=find_packages(exclude=['tests', 'tests.*']),
     install_requires=[
         'PyCrypto >= 2.6.1',
     ],
+    keywords=['minecraft'],
     classifiers=[
         'License :: OSI Approved :: MIT License',
     ]
